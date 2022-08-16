db28027ff55c0abb0d4118b54f46e64baa908a55
Pavel <archangel-44@mail.ru>
2022-08-16 19:31:03 +0300

    3-ДЗ готово

--- Program.cs
+++ Program.cs
@@ -14,9 +14,9 @@ void CheckingNumber(string number)
     {
         if (number[0]==number[4] || number[1]==number[3])
         {
-        Console.WriteLine($"Ваше число: {number} - является палиндром.");
+        Console.WriteLine($"Ваше число: {number} -  палиндром.");
         }
-        else Console.WriteLine($"Ваше число: {number} - НЕ является палиндром.");
+        else Console.WriteLine($"Ваше число: {number} - НЕ палиндром.");
     }
 
     if (number!.Length == 5)
